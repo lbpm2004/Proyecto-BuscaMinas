04300f78cccaa0c2250bb9d2c2fce77d082f123b
fabianarp <167768625+fabianarp@users.noreply.github.com>
2025-03-08 17:45:34 -0400

    Update Tablero.java

--- BuscaMinasProyecto/src/buscaminasproyecto/Tablero.java
+++ BuscaMinasProyecto/src/buscaminasproyecto/Tablero.java
@@ -14,29 +14,42 @@ public class Tablero {
     private int filas; //num de filas del tablerp
     private int columnas; //num de columnas del tablero
     private int minas; //num de minas en el tablero
-    private boolean[][] minasLugar; //matriz para saber si hay una mina en las casillas
-    private int[][] cantidadMinas; //matriz con el conteo de minas adyacentes de las casillas
-    private boolean[][] encontradas; //matriz que dice si una casilla fue descubierta
+    private List<List<Boolean>> minasLugar; //lista de listas para las minas
+    private List<List<Interger>> cantidadMinas; //lista de listas para la cantidad de minas
+    private List<List<Boolean>> encontradas; //Lista de listas para las casillas encontradas
     
 public Tablero(int filas, int columnas, int minas){
     this.filas = filas;
     this.columnas = columnas;
     this.minas = minas;
-    this.minasLugar = new boolean[filas][columnas]; //Inicializa la matriz
-    this.cantidadMinas = new int [filas][columnas]; //Inicializa la matriz
-    this.encontradas = new boolean [filas][columnas]; //Inicializa la matriz
-    this.ponerMinas(); //pone las minas en el tablero
+    minasLugar = new ArrayList<>(); //Inicializa la lista de listas
+    cantidadMinas =new ArrayList<>(); //Inicializa la lista de listas
+    encontradas = new ArrayList<>(); //Inicializa la lista de listas
+
+    for(int i = 0; i < filas; i++) { //i es el indice de la fila actual //itera sobre todas las filas del tablero //va desde 0 hasta la fila -1 (recorre todas las filas del tablero)
+        minasLugar.add(new ArrayList<>(columnas)); //nueva lista array que representa una fila para las minas //tamaño de columnas que se llena en el siguiente bucle
+        cantidadMinas.add(new ArrayList<>(columnas)); //lista array para guardar la cantidad de minas adyacentes a i
+        encontradas.add(new ArrayList<>(columnas)); //para saber si todas las casillas de la fila han sido encontradas
+        for (int j = 0; j < columnas; j++) { //j es el indice de la columna actual // itera sobre todas las columnas del tablero //va desde 0 a -1 (recorre todas las columnas del tablero)
+            minasLugar.get(i).add(false); //al principio no hay minas en esa casilla
+            cantidadMinas.get(i).add(0); //al principio no se ha calculado la cantidad de minas adyacentes a esa casilla
+            encontradas.get(i).add(false) //al principio no se ha encontrado la casilla
+                }
+    }
+    
+    ponerMinas(); //pone las minas en el tablero
     calcularCantidadMinas(); //calcula la cantidad de minas adyacentes
+    //son metodos
 }
 
 private void ponerMinas() {
     Random random = new Random(); //generador de numeros aleatorios
-    int minasPuestas = 0; //cantidad de minas puestas en el tablero
+    int minasPuestas = 0; //cantidad de minas puestas en el tablero (empieza en 0)
     while (minasPuestas < minas) { //mientras que minasPuestas sea menor que las minas en el tablero
-        int fila = random.nextInt(filas); //Se crea una fila aleatoria
-        int columna = random.nextInt(columnas); //Se crea una columna aleatoria
-        if (!minasLugar[fila][columna]) { //Ver si no hay minas en esa posicion
-            minasLugar[fila][columna] = true; //Pone la mina
+        int fila = random.nextInt(filas); //Se crea una fila aleatoria (en la que se tratara de poner la mina)
+        int columna = random.nextInt(columnas); //Se crea una columna aleatoria (en la que se tratara de poner la mina)
+        if (!minasLugar.get(fila).get(columna)) { //Ver si no hay minas en esa posicion (true = si hay mina, false = no hay mina)
+            minasLugar.get(fila).set(columna, true); //si no habia mina se coloca una y se cambia el valor a true en la lista de minasLugar
             minasPuestas++; //aumenta la cantidad de minas puestas en el tablero
         }
     }       
@@ -46,7 +59,7 @@ private void ponerMinas() {
 private void calcularCantidadMinas() {
     for (int i = 0; i < filas; i++) { //Recorre cada fila  //i es el indice de la fila actual
         for (int j = 0; j < columnas; j++) { //Recorre cada columna  //j es el indice de la columna actual
-            if (minasLugar[i][j]) { //Si hay una mina en la posicion en la que estamos
+            if (minasLugar.get(i).get(j)) { //Si hay una mina en la posicion en la que estamos
                 for (int x = -1; x <= 1; x++) { //Recorre las filas adyacentes de la que estamos 
                     //x puede tomar valores de -1, 0 y 1 y asi puedes ver la casilla de arriba, la actual y la de abajo
                     for (int y = -1; y <= 1; y++) { //Lo mismo pero con las columnas  //puedes acceder a la de la izquierda, la actual y la de la derecha
@@ -57,7 +70,7 @@ private void calcularCantidadMinas() {
                         int nuevaColumna = j + y; //calcula la nueva columna (calcula la columna de la casilla adyacente sumandole y a j)
                         //VERIFICAR QUE ESTE DENTRO DEL TABLERO
                         if (nuevaFila >= 0 and nuevaFila < filas and nuevaColumna >= 0 and nuevaColumna < columnas) {
-                            cantidadMinas[nuevaFila][nuevaColumna]++; //Si esta dentro del tablero, aumenta el conteo de mias adyacentes de cantidadMinas
+                            cantidadMinas.get(nuevaFila).set(nuevaColumna, cantidadMinas.get(nuevaFila).get(nuevaColumna) +1); //Si esta dentro del tablero, aumenta el conteo de mias adyacentes de cantidadMinas
                         }
                         
                     }
@@ -69,7 +82,7 @@ private void calcularCantidadMinas() {
 */
 
 public boolean barrerCasilla(int fila, int columna) { //devuelve un boolean y le pasamos fila y columna
-    if (minasLugar[fila][columna]) { //ver si hay una mina en la casilla indicada por fila y columna 
+    if (minasLugar.get(fila).get(columna)) { //ver si hay una mina en la casilla indicada por fila y columna 
         //minasLugar tiene los booleamos que dicen si hay una mina en las casillas
         return false; //Cuando se descubre una mina
     } else {
@@ -79,23 +92,27 @@ public boolean barrerCasilla(int fila, int columna) { //devuelve un boolean y le
 
 private void encontrarCasilla(int fila, int columna) { //recibe fila y columna
     //encontradas siempre es true entonces cuando retorne false es que no ha sido encontrada
-    if (encontradas[fila][columna]) return; //Ver si ya fue encontrada
-    encontradas[fila][columna] = true; //Marcar como encontrada
-}
+    if (encontradas.get(fila).get(columna)) return; //Ver si ya fue encontrada
+    encontradas.get(fila).set(columna, true); //Marcar como encontrada
 
+    //Si la cantidad de minas adyacentes es 0, encontrarlas
+    if (cantidadMinas.get(fila).get(columna) == 0) { //ver si la cantidad de minas adyacentes es 0
+        for (int x = -1; x <= 1; x++) { //itera sobre las minas adyacentes de la fila actual // x puede tomas valores de -1, 0 y 1 (casilla de arriba, actual y de abajo)
+            for (int y = -1; y <= 1; y++) { //lo mismo pero con las columnas
+                if (x == 0 and y == 0) //ver si estamosen la  misma casilla (si los dos son 0)
+                    continue; //se ignora esa iteracion si es la misma casilla
+                int nuevaFila = fila + x; //calcula la fila de la casilla adyacente (se le suma x a la fila)
+                int nuevaColumna = columna + y; //calcula la columna de la casilla adyacente (se le suma y a la columna)
+                //ver si la nueva posicion esta EN EL TABLERO
+                if (nuevaFila >= 0 and nuevaFila < filas and nuevaColumna >= 0 and nuevaColumna <  columnas) { //ver si la nuevaFila esta entre 0 y la fila -1 y que la nuevaColumna este entre 0 y la columna -1
+                    encontrarCasilla(nuevaFila, nuevaColumna); //si la nueva posicion es valida se llama al metodo encontrarCasilla para encontrar la adyacente
+                }
+            }
+        }
+    }
+
+    
 //Getters
-public int getCantidadMinas(int fila, int columna) {
-    return cantidadMinas[fila][columna]; //cantidad de minas adyacentes
-}
-
-public boolean isCasillaEncontrada(int fila, int columna) {
-    return encontradas[fila][columna]; //Si la casilla fue descubierta o no
-}
-
-public boolean isCasillaConMina(int fila, int columna) {
-    return minasLugar[fila][columna]; //Si hay o no una mina en la casilla
-}
-
 public int getFilas() {
     return filas; //Numero de filas
 }
@@ -103,5 +120,23 @@ public int getFilas() {
 public int getColumnas() {
     return columnas; //Numero de columnas
 }
+    
+public int getMinas() {
+    return minas; //Numero de minas
+}
+    
+public int getCantidadMinas(int fila, int columna) {
+    return cantidadMinas.get(fila).get(columna); //cantidad de minas adyacentes
+}
+
+public boolean isCasillaEncontrada(int fila, int columna) {
+    return encontradas.get(fila).get(columna); //Si la casilla fue descubierta o no
+}
+
+public boolean isCasillaConMina(int fila, int columna) {
+    return minasLugar.get(fila).get(columna); //Si hay o no una mina en la casilla
+}
+
+
 }
 
